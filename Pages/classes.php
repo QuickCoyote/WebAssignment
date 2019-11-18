2bc13cf78748d0c73f07361aab5075dafdf54e9c
Joseph Baja <jbaja@student.neumont.edu>
2019-11-18 11:11:12 -0700

    base pages

---
+++ Pages/classes.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href='style2.css' rel='stylesheet' />
+</head>
+<body>
+    <div class='navbar'>
+        <ul>
+            <li>
+                <a href='index.php'>HOME</a>
+            </li> 
+            <li>
+                <a href='classes.php'>CLASSES</a>
+            </li> 
+            <li>
+                <a href='spells.php'>SPELLS</a>
+            </li> 
+        </ul>
+</div>
+</body>
+</html>
