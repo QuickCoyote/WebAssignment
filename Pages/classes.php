8fa0b9d4dd3d6ac4c3be38a250ab586dce902290
Joseph Baja <jbaja@student.neumont.edu>
2019-11-18 11:51:51 -0700

    dropdowns

--- Pages/classes.php
+++ Pages/classes.php
@@ -5,20 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href='../style.css' rel='stylesheet' />
 </head>
 <body>
     <div class='navbar'>
-        <ul>
-            <li>
-                <a href='../index.php'>HOME</a>
-            </li> 
-            <li>
-                <a href='classes.php'>CLASSES</a>
-            </li> 
-            <li>
-                <a href='spells.php'>SPELLS</a>
-            </li> 
-        </ul>
+            <a href='../index.php'>HOME</a>
+            <div class="dropdown">
+                <button class="dropbtn">Classes
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">CLASS 1</a>
+                    <a href="#">CLASS 2</a>
+                    <a href="#">CLASS 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Spells
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">SPELL 1</a>
+                    <a href="#">SPELL 2</a>
+                    <a href="#">SPELL 3</a>
+                </div>
+            </div>
     </div>
 </body>
 </html>
