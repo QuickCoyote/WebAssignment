22bc293f02e27c780c551be9c158170c0a4108f4
Unknown <ruppertjarrett@yahoo.com>
2019-11-24 16:31:50 -0700

    Styling stuff

---
+++ styleSwap.js
@@ -0,0 +1,17 @@
+function cssToRed() {
+    var cssLink = document.getElementById('cssLink');
+
+    cssLink.setAttribute('href', './Styles/redtheme.css');
+}
+
+function cssToBrick() {
+    var cssLink = document.getElementById('cssLink');
+
+    cssLink.setAttribute('href', './Styles/bricktheme.css');
+}
+
+function cssToNeutral() {
+    var cssLink = document.getElementById('cssLink');
+
+    cssLink.setAttribute('href', './Styles/neutraltheme.css');
+}
